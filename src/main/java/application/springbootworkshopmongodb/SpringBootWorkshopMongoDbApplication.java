8e424fbac52a1cfc7166e5b8381d067a585e1616
Admar Junior <admarjunior56@gmail.com>
2024-10-25 17:29:05 -0300

    first commit

---
+++ src/main/java/application/springbootworkshopmongodb/SpringBootWorkshopMongoDbApplication.java
@@ -0,0 +1,13 @@
+package application.springbootworkshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWorkshopMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWorkshopMongoDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/application/springbootworkshopmongodb/SpringBootWorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package application.springbootworkshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
